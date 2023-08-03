3fdbdf182521fb2d5d15b9a6951ec15615d3bfa5
Miguel <Miguel@impacts.cloud>
2023-08-03 16:41:07 +0800

    加入專案檔案。

---
+++ Amazon/Controllers/CategoryController.cs
@@ -0,0 +1,39 @@
+﻿using Amazon.Data;
+using Amazon.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Amazon.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> obj = _db.Categories.ToList();
+            return View(obj);
+        }
+        public IActionResult Create() {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if(obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name.");
+            }
+            if(ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+    }
+}
