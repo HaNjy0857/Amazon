19895dad331c2f423ec5cf86dbc229ca38f42dd5
kkeekeke <asd@jmail.com>
2023-08-23 00:07:39 +0800

    cp52

--- Amazon/Controllers/CategoryController.cs
+++ Amazon/Controllers/CategoryController.cs
@@ -31,6 +31,7 @@ namespace Amazon.Controllers
             {
                 _db.Categories.Add(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category created successfully";
                 return RedirectToAction("Index");
             }
             return View(obj);
@@ -44,16 +45,39 @@ namespace Amazon.Controllers
             }
             return View(result);
         }
-        [HttpPut]
+        [HttpPost]
         public IActionResult Edit(Category obj)
         {
             if (ModelState.IsValid)
             {
                 _db.Categories.Update(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category updated successfully";
                 return RedirectToAction("Index");
             }
             return View();
-        }
-    }
+		}
+		public IActionResult Delete(int? id)
+		{
+			var result = _db.Categories.FirstOrDefault(x => x.Id == id);
+			if (result == null)
+			{
+				return NotFound();
+			}
+			return View(result);
+		}
+		[HttpPost, ActionName("Delete")]
+		public IActionResult DeletePost(int? id)
+		{
+            Category? obj = _db.Categories.Find(id);
+            if(obj == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(obj);
+			_db.SaveChanges();
+            TempData["success"] = "Category deleted successfully";
+            return RedirectToAction("Index");
+		}
+	}
 }
